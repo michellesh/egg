192b637f7e16ac9808bece4dd6dc0daaf39df746
Michelle Sharer <michellesharer@gmail.com>
2023-03-07 21:48:32 -0700

    Two metaball blobs in a for loop

--- eggv2/metaballs.ino
+++ eggv2/metaballs.ino
@@ -1,9 +1,11 @@
+#define NUM_BLOBS 2
+
 struct Blob {
   float radius;
   float angle;
   float height;
-  int speedHorizontal;
-  int speedVertical;
+  float speedHorizontal;
+  float speedVertical;
 
   void update() {
     angle += speedHorizontal;
@@ -17,22 +19,27 @@ struct Blob {
   }
 };
 
-Blob b = {20, 180, 50, 1, 1};
-Blob b2 = {20, 90, 100, 1, 1};
+Blob b = {20, 180, 50, 0.5, 0.5};
+Blob b2 = {20, 90, 100, 0.5, 0.5};
+Blob blobs[NUM_BLOBS] = {b, b2};
 
 void metaballs() {
   for (int r = 0; r < NUM_RINGS; r++) {
     for (int i = 0; i < rings[r].numLEDs; i++) {
-      float dist =
-          distance(rings[r].angle[i], rings[r].height, b.angle, b.height);
-      float dist2 =
-          distance(rings[r].angle[i], rings[r].height, b2.angle, b2.height);
-      int brightness = 100 * b.radius / dist;
-      brightness += 100 * b2.radius / dist2;
-      brightness = constrain(brightness, 0, 255);
-      rings[r].leds[i] = CHSV(HUE_BLUE, 100, brightness);
+
+      int sum = 0;
+      float dist;
+
+      for (int k = 0; k < NUM_BLOBS; k++) {
+        dist = distance(rings[r].angle[i], rings[r].height, blobs[k].angle,
+                        blobs[k].height);
+        sum += 100 * blobs[k].radius / dist;
+      }
+
+      sum = constrain(sum, 0, 255);
+      rings[r].leds[i] = CHSV(HUE_BLUE, 100, sum);
     }
   }
-  b.update();
-  b2.update();
+  blobs[0].update();
+  blobs[1].update();
 }
